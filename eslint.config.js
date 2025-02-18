585dfc2dd210586d2369714cab5c04d76603de24
Neil Enns <neile@live.com>
2025-02-17 18:35:52 -0800

    Initial version (#1)
    * Initial version
    
    * Fix build issues
    
    * Add basic coderabbit config
    
    * Fix break again
    
    * More build failures

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import pluginJs from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  prettierConfig,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+];
